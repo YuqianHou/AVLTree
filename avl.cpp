c850c8f3a102776587f50088c4bfed6416d4bfb6
YuqianHou <50307487+YuqianHou@users.noreply.github.com>
2019-06-05 22:06:57 -0700

    Add files via upload

--- avl.cpp
+++ avl.cpp
@@ -96,28 +96,31 @@ int main(int argc, char* argv[])
                     if (t.countLL == 0) {
                         cout << "No inserts would cause a left-left rotation." << endl;
                     }
+                    cout << endl;
                 }else if (command == "left-right"){
                     t.countLR = 0;
                     t.left_right();
                     if (t.countLR == 0) {
                         cout << "No inserts would cause a left-right rotation." << endl;
                     }
+                    cout << endl;
                 }else if (command == "right-left"){
                     t.countRL = 0;
                     t.right_left();
                     if (t.countRL == 0) {
                         cout << "No inserts would cause a right-left rotation." << endl;
                     }
+                    cout << endl;
                 }else if (command == "right-right"){
                     t.countRR = 0;
                     t.right_right();
                     if (t.countRR == 0) {
                         cout << "No inserts would cause a right-right rotation." << endl;
                     }
+                    cout << endl;
                 }else{
                     cout << "Wrong command" << endl;
                 }
-                cout << endl;
             }
         }else{
             cout << "Wrong command" << endl;
